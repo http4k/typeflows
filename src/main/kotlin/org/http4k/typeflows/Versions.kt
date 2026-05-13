55abb906eab7e3e1eb667cf74d6a48006de5488b
David Denton <daviddenton@users.noreply.github.com>
2026-05-13 07:47:23 +0100

    Upgrade versions

--- src/main/kotlin/org/http4k/typeflows/Versions.kt
+++ src/main/kotlin/org/http4k/typeflows/Versions.kt
@@ -4,7 +4,6 @@ import io.typeflows.github.workflow.step.marketplace.JavaDistribution.Temurin
 import io.typeflows.github.workflow.step.marketplace.JavaVersion.V21
 
 object Versions {
-    const val GRADLE = "9.3.1"
     val JDK = Temurin
     val JAVA_VERSION = V21
 }
